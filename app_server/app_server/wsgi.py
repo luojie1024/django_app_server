efb4f81a2547d3ce15588fdf99a908d7a9f074f3
luojie1024 <luojie199403@126.com>
2017-08-09 13:23:03 +0800

    阿里云部署

--- app_server/app_server/wsgi.py
+++ app_server/app_server/wsgi.py
@@ -1,4 +1,4 @@
-"""
+"pp_server""
 WSGI config for app_server project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app_server.settings")
 
 application = get_wsgi_application()
+sys.path.append("/var/www/django_app_server/app_server/")
