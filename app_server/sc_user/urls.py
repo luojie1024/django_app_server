9e02b9427ee59564961ec14801dd574a80fc2e95
jacky <luojie199403@126.com>
2017-09-08 20:31:42 -0700

    updata_urls

--- app_server/sc_user/urls.py
+++ app_server/sc_user/urls.py
@@ -7,9 +7,6 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.login_handle),
     url(r'^login/(?P<pk>[0-9]+)/$', views.login_old),
     url(r'^', views.login),
-    # url(r'^getUserList/$', views.getUserList),
-    # url(r'^setUser/$', views.setUser),
     url(r'^snippets/$', views.snippet_list),
-    # url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
 
