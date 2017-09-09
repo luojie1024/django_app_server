93dde58cc5a6df94b1161bad512e923bce62c731
jacky <luojie199403@126.com>
2017-09-08 20:19:27 -0700

    Admin更新

--- app_server/app_server/settings.py
+++ app_server/app_server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c5p8^411q^og($54ti$(^_3z5%0#&i3c%&9@t*32z%f*41$2!6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', ]
 
--- app_server/sc_device/admin.py
+++ app_server/sc_device/admin.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import DeviceInfo
 
 # Register your models here.
+@admin.register(DeviceInfo)
+class GatewayInfoAdmin(admin.ModelAdmin):
+    # 自定义显示
+    list_display = ['id', 'dtype','dname','dmac', 'dbrand', 'gcontrolcode', 'ggateway']
+    # 过滤器
+    list_filter = ['dname']
+    # 搜索功能
+    search_fields = ['dname']
+    # 每页显示多少条数据
+    list_per_page = 10
+    # 修改,添加页分组
+    fieldsets = [('base', {'fields': ['dname']}),]
